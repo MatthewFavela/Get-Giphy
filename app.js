31129a782fb88ce252741fb01bb3a64c39be1f49
MatthewFavela <mfavela3508@myhta.org>
2018-05-29 14:17:52 -1000

    giphy api

---
+++ app.js
@@ -0,0 +1,30 @@
+    var form = document.getElementById("main");
+    var input = document.getElementById("input");
+
+    form.addEventListener("submit", function (event){
+        event.preventDefault();
+
+        var result = input.value;
+
+    function eventListener(){
+        var data = JSON.parse(this.responseText);
+        console.log('data',data)
+        var div = document.getElementById("images");
+
+        for(var i=0;i<5;i++){
+            var newImg = document.createElement('img');
+            newImg.src =  data.data[i].images.original.url;
+            div.appendChild(newImg);
+    }
+}
+var req = new XMLHttpRequest;
+    req.addEventListener("load", eventListener);
+    req.open("GET", "http://api.giphy.com/v1/gifs/search?q=" + result + "&api_key=whkBthtmqmKvXxWW9CRnz72McvYLylVJ&limit=5");
+    req.send()
+    });
+
+
+
+
+
+
